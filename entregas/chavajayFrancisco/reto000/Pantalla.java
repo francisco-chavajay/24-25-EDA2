a3bad522b000c53133575775fbc6e6cdb48c179b
francisco-chavajay <juan.chavajay@uneatlantico.es>
2025-02-09 21:25:16 +0100

    entrega de primer reto

---
+++ entregas/chavajayFrancisco/reto000/Pantalla.java
@@ -0,0 +1,49 @@
+import java.util.List;
+
+class Pantalla {
+    private String nombre;
+    private Clase[] clases;
+
+    public void setFrames() {
+    }
+}
+
+class Buffer {
+    public void mostrarFrameElegido(Frame1 frame1, Frame2 frame2, ResultadoVisual resultado) {
+    }
+}
+
+class Pixel {
+    private char[][] matriz;
+    private Clase clase;
+    private List<Object> elemento;
+}
+
+class ColoresCompuestos {
+    private char[][] matriz;
+    private Clase clase;
+    private List<Object> colores;
+}
+
+class Frame1 {
+    private char[][] matriz;
+    private Clase clase;
+    private Object posicionFrames;
+}
+
+class Frame2 {
+    private char[][] matriz;
+    private Clase clase;
+    private Object posicionFrames;
+}
+
+class ResultadoVisual {
+    private char[][] matriz;
+    private Clase clase;
+
+    public void combinar(Frame1 frame1, Frame2 frame2) {
+    }
+}
+
+class Clase {
+}
